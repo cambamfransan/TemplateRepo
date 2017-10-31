6d442f73a36a49c01122c38e69f39d739e15dd71
Cameron Frandsen <cameron.frandsen1@gmail.com>
2017-10-31 10:44:35 -0600

    add qt and ctest

---
+++ Shared/ProtoFiles/test/ProtoFilesTestMain.cpp
@@ -0,0 +1,52 @@
+#include <QtTest>
+#include "ProtoFiles/AddressBook.pb.h"
+
+class ProtoFilesTest : public QObject
+{
+  Q_OBJECT
+
+public:
+  ProtoFilesTest();
+  ~ProtoFilesTest();
+
+  private slots:
+  void initTestCase();
+  void cleanupTestCase();
+  void checkAddressBook();
+};
+
+ProtoFilesTest::ProtoFilesTest()
+{
+}
+
+ProtoFilesTest::~ProtoFilesTest()
+{
+}
+
+void ProtoFilesTest::initTestCase()
+{
+}
+
+void ProtoFilesTest::cleanupTestCase()
+{
+  google::protobuf::ShutdownProtobufLibrary();
+}
+
+void ProtoFilesTest::checkAddressBook()
+{
+  tutorial::Person person;
+  person.set_email("cameron.frandsen1@gmail.com");
+  person.set_id(1);
+  person.set_name("Cameron Frandsen");
+
+  auto sString = person.SerializeAsString();
+  tutorial::Person returnedMsg;
+  returnedMsg.ParseFromString(sString);
+  QVERIFY(returnedMsg.id() == 1);
+  QVERIFY(returnedMsg.email() == "cameron.frandsen1@gmail.com");
+  QVERIFY(returnedMsg.name() == "Cameron Frandsen");
+}
+
+QTEST_APPLESS_MAIN(ProtoFilesTest)
+
+#include "ProtoFilesTestMain.moc"
