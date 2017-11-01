54ee2dedf7af67c67f8ae9b1f5de627a74e3a46e
Cameron Frandsen <cameron.frandsen1@gmail.com>
2017-10-31 19:59:47 -0600

    see if the gets rid of the link errors

--- Executable/main.cpp
+++ Executable/main.cpp
@@ -4,7 +4,7 @@
 #include "ProtoFiles/addressbook.pb.h"
 
 void promptForAddress(tutorial::Person* person) {
-  std::cout << "Enter person ID number: ";
+ /* std::cout << "Enter person ID number: ";
   int id;
   std::cin >> id;
   person->set_id(id);
@@ -44,7 +44,7 @@ void promptForAddress(tutorial::Person* person) {
     } else {
       std::cout << "Unknown phone type. Using default." << std::endl;
     }
-  }
+  }*/
 }
 
 #include <iostream>
@@ -66,31 +66,31 @@ int main(int argc, char * argv[])
     return -1;
   }
 
-  tutorial::AddressBook address_book;
-  {
-    std::fstream input(argv[1], std::ios::in | std::ios::binary);
-    if(!input) 
-    {
-      std::cout << argv[1] << ": File not found. Creating a new file." <<
-        std::endl;
-    }
-    //else if (!address_book.ParseFromIstream(&input)) 
-    //{
-    //  std::cout << "Failed to parse address book." << std::endl;
-    //  return -1;
-    //}
-  }
-  //std::cout << address_book.DebugString() << std::endl;
-
-  promptForAddress(address_book.add_people());
-
-  std::fstream output(argv[1], std::ios::out | std::ios::trunc | std::ios::binary);
-  //if(!address_book.SerializeToOstream(&output))
+  //tutorial::AddressBook address_book;
   //{
-  //  std::cerr << "Failed to write address book. " << std::endl;
-  //  return -1;
+  //  std::fstream input(argv[1], std::ios::in | std::ios::binary);
+  //  if(!input) 
+  //  {
+  //    std::cout << argv[1] << ": File not found. Creating a new file." <<
+  //      std::endl;
+  //  }
+  //  //else if (!address_book.ParseFromIstream(&input)) 
+  //  //{
+  //  //  std::cout << "Failed to parse address book." << std::endl;
+  //  //  return -1;
+  //  //}
   //}
-  //google::protobuf::ShutdownProtobufLibrary();
+  ////std::cout << address_book.DebugString() << std::endl;
+
+  //promptForAddress(address_book.add_people());
+
+  //std::fstream output(argv[1], std::ios::out | std::ios::trunc | std::ios::binary);
+  ////if(!address_book.SerializeToOstream(&output))
+  ////{
+  ////  std::cerr << "Failed to write address book. " << std::endl;
+  ////  return -1;
+  ////}
+  ////google::protobuf::ShutdownProtobufLibrary();
 
   return 0;
 }
